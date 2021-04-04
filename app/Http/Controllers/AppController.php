a76b1820e59222fdd57a83e17d0b32ed7cc1a751
kozima <gukozima@gmail.com>
2021-04-04 17:08:29 +0100

    Some error handling and output format

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -10,20 +10,19 @@ class AppController extends Controller
     private $officeLocation = [53.3340285, -6.2535495];
     //
     public function getAffiliates($filename = 'affiliates.txt'){
-        //open file
-        $file = fopen($this->path.$filename, "r");
         $affiliates = [];
-        if ($file) {
+        //open file
+        if($file = fopen($this->path.$filename, "r")){
             //read file line by line and convert from json to array
             while (($line = fgets($file)) !== false) {
                 $row = (array)json_decode($line);
                 $affiliates[$row['affiliate_id']] = $row;
             }
-
             fclose($file);
-        } else {
-            throw new Exception('File not found.');
+        }else{
+            throw new Exception("File not found on ".getcwd( ).".");
         }
+        
         // sort array by id ASC
         ksort($affiliates); 
         // call function to search nearbyAffiliates
@@ -35,23 +34,28 @@ class AppController extends Controller
         foreach($affiliates as $a){
             // calculate distance between each affiliate and our office
             if($this->gcd([$a['latitude'],$a['longitude']],$this->officeLocation)<$distance){
-                $nearby[] = $a;
+                $result['affiliate_id'] = $a['affiliate_id'];
+                $result['name'] = $a['name'];
+                $nearby[] = $result;
             }
         }
-        return $nearby;
+        return json_encode($nearby);
     }
     
     //this function returns the distance in Km
     //point structure = [lat,lon]
     private function gcd($point1, $point2){
-        $theta = $point1[1] - $point2[1];
-
+        if(!isset($point1) || !isset($point2)){
+            throw new Exception("Missing parameters.");
+        }
         //Great-circle distance formula
+        $theta = $point1[1] - $point2[1];
         $dist = sin(deg2rad($point1[0])) * sin(deg2rad($point2[0])) +  cos(deg2rad($point1[0])) * cos(deg2rad($point2[0])) * cos(deg2rad($theta));
 
         //this is to avoid to cause NaN errors when rounding $dist get bigger than 1 or less than -1 
         $dist = acos(min(max($dist,-1.0),1.0)); 
         $dist = rad2deg($dist);
+        //distance in Km
         $distance = ($dist * 60 * 1.1515)* 1.609344;
         return $distance;
     }
