2d3ce808f22565cbf00504ae3fd161e3b4b03c63
kozima <gukozima@gmail.com>
2021-04-04 17:45:07 +0100

    Fix for file not found

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -6,11 +6,10 @@ use Illuminate\Http\Request;
 
 class AppController extends Controller
 {
-    private $path = '/app/storage/app/';
+    private $path = '../storage/app/';
     private $officeLocation = [53.3340285, -6.2535495];
     public function getAffiliates($filename = 'affiliates.txt'){
         $affiliates = [];
-        return dirname(__FILE__);
         //open file
         try {
             $file = fopen($this->path.$filename, "r");
